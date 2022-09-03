88c3ddbfb7e9001003d8c8a26935c5911dfa0a7c
Dhamareshwarakumar <damareshwarakumar@gmail.com>
2022-09-03 18:30:31 +0530

    [Backend] Completed Groups API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 import store from './config/store';
 import setAuthToken from './config/setAuthToken';
@@ -16,25 +17,28 @@ import Login from './pages/Login';
 import Logout from './pages/Logout';
 
 
-// Checking if the user is authenticated
-if (localStorage.jwtToken) {
-	// Set Authorization Header
-	setAuthToken(localStorage.jwtToken);
-
-	// Decode token and get user info
-	const decoded = jwt_decode(localStorage.jwtToken);
-	// Set Current User
-	store.dispatch(setCurrentUser(decoded));
-
-	// Check for expired token
-	const currentTime = Date.now() / 1000;
-	if (decoded.exp < currentTime) {
-		// Logout user
-		store.dispatch(logout());
-	}
-}
-
 const App = () => {
+	const navigate = useNavigate();
+
+	// Checking if the user is authenticated
+	if (localStorage.jwtToken) {
+		// Set Authorization Header
+		setAuthToken(localStorage.jwtToken);
+
+		// Decode token and get user info
+		const decoded = jwt_decode(localStorage.jwtToken);
+		// Set Current User
+		store.dispatch(setCurrentUser(decoded));
+
+		// Check for expired token
+		const currentTime = Date.now() / 1000;
+		if (decoded.exp < currentTime) {
+			// Logout user
+			store.dispatch(logout(navigate));
+		}
+	}
+
+
 	return (
 		<Provider store={store}>
 			<Navbar />
